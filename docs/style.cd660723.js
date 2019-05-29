8fcebf289ef3b44d71dbebc5106a0617a3dd632d
HELEMAAL_NIKS <jaron@hmcnetwork.nl>
2019-05-29 10:41:53 +0200

    npm run build gedaan om conflict te resolven

--- docs/style.cd660723.js
+++ docs/style.cd660723.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52324" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52472" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
