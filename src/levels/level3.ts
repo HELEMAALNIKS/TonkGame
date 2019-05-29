60cde9ab6333ef1468f6e433c2cbee75785d44e6
HELEMAAL_NIKS <jaron@hmcnetwork.nl>
2019-05-29 12:04:05 +0200

    Level 2 en 3 aangepast

--- src/levels/level3.ts
+++ src/levels/level3.ts
@@ -1,5 +1,17 @@
 import { GameScene } from "../scenes/game-scene"
+import { Scenes } from "phaser";
 export class Level3 extends Phaser.Scene { 
+
+    private gamescene: GameScene
+
+    constructor() {
+        super({ key: "Level3" })
+    }
+    
+    create(): void {
+        this.gamescene = new GameScene()
+        console.log("wekrt dit?")
+    }
     
 }
 
